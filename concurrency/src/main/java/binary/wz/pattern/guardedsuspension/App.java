e13e3b44d639aa5012f2f135f6ceb52e5820f7d1
binarywz <binarywz@163.com>
2022-01-26 22:35:31 +0800

    Guarded Suspension

--- concurrency/src/main/java/binary/wz/pattern/guardedsuspension/App.java
+++ concurrency/src/main/java/binary/wz/pattern/guardedsuspension/App.java
@@ -2,30 +2,49 @@ package binary.wz.pattern.guardedsuspension;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.util.List;
-
 /**
  * @author binarywz
  * @date 2022/1/25 23:05
- * @description:
+ * @description: Postman(结果产生线程)跟People(结果接收线程)是一一对应的关系
  */
 @Slf4j
 public class App {
-    public static void main(String[] args) {
-        GuardedObject guardedObject = new GuardedObject();
-        new Thread(() -> {
-            try {
-                List<String> response = Downloader.download();
-                log.debug("download complete...");
-                guardedObject.execute(response);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start();
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 3; i++) {
+            new People().start();
+        }
+        Thread.sleep(1_000);
+        for (Integer id : GuardManager.getIds()) {
+            new Postman(id, "postman-" + id).start();
+        }
+    }
+}
 
-        log.debug("waiting...");
-        Object response = guardedObject.get();
-        log.debug("get response: {}", response);
+@Slf4j
+class People extends Thread {
+    @Override
+    public void run() {
+        GuardedObject guardedObject = GuardManager.createGuardedObject();
+        log.debug("等待收信, id: {}", guardedObject.getId());
+        Object res = guardedObject.get(5_000);
+        log.debug("收到信, id: {}, content: {}", guardedObject.getId(), res);
+    }
+}
+
+@Slf4j
+class Postman extends Thread {
+    private int id;
+    private String mail;
+
+    public Postman(int id, String mail) {
+        this.id = id;
+        this.mail = mail;
+    }
+
+    @Override
+    public void run() {
+        GuardedObject guardedObject = GuardManager.getGuardedObject(this.id);
+        log.debug("开始送信, id: {}, content: {}", guardedObject.getId(), mail);
+        guardedObject.complete(mail);
     }
 }
--- concurrency/src/main/java/binary/wz/pattern/guardedsuspension/GuardedObject.java
+++ concurrency/src/main/java/binary/wz/pattern/guardedsuspension/GuardedObject.java
@@ -7,25 +7,43 @@ package binary.wz.pattern.guardedsuspension;
  */
 public class GuardedObject {
     /**
+     * 标识id
+     */
+    private int id;
+    /**
      * 结果
      */
     private Object response;
 
+    public GuardedObject(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
     /**
      * 获取结果
+     * @param millis
      * @return
      */
-    public Object get() {
+    public Object get(long millis) {
         synchronized (this) {
-            /**
-             * 没有结果则一直循环等待结果
-             */
+            long base = System.currentTimeMillis(); // 开始时间
+            long now = 0; // 经历的时间
+            // 没有结果则一直循环等待结果
             while (response == null) {
+                long delay = millis - now;
+                if (delay <= 0) {
+                    break;
+                }
                 try {
-                    this.wait();
+                    this.wait(delay);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                now = System.currentTimeMillis() - base;
             }
             return response;
         }
@@ -34,7 +52,7 @@ public class GuardedObject {
     /**
      * 产生结果
      */
-    public void execute(Object response) {
+    public void complete(Object response) {
         synchronized (this) {
             this.response = response;
             this.notifyAll();
