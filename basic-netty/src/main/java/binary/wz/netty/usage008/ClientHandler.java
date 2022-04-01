ea9ad49608c1f04a98009ca4473ccd6010aef7da
binarywz <binarywz@163.com>
2022-04-01 22:28:33 +0800

    netty usage009

---
+++ basic-netty/src/main/java/binary/wz/netty/usage008/ClientHandler.java
@@ -0,0 +1,67 @@
+package binary.wz.netty.usage008;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.socket.SocketChannel;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author binarywz
+ * @date 2022/4/1 21:25
+ * @description:
+ */
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 客户端与服务端建立连接成功
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        SocketChannel channel = (SocketChannel) ctx.channel();
+        System.out.println("连接报告开始");
+        System.out.println("连接到服务端，channelId: " + channel.id());
+        System.out.println("连接报告IP: " + channel.localAddress().getHostString());
+        System.out.println("连接报告Port: " + channel.localAddress().getPort());
+        System.out.println("连接报告完毕");
+    }
+
+    /**
+     * 客户端主动断开与服务端的连接
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(ctx.channel().localAddress().toString() + "断开连接...");
+    }
+
+    /**
+     * 处理消息
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // 处理消息
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .format(new Date()) + " 接收到消息: " + msg);
+        String content = new Date() + " " + ctx.channel().localAddress().toString() + " " + msg + "\r\n";
+        ctx.writeAndFlush(content);
+    }
+
+    /**
+     * 捕获异常，发生异常进行一些处理
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+        System.out.println("异常信息: \r\n" + cause.getMessage());
+    }
+}
---
+++ basic-netty/src/main/java/binary/wz/netty/usage009/LengthFieldDecoderUsage.java
@@ -0,0 +1,46 @@
+package binary.wz.netty.usage009;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author binarywz
+ * @date 2022/4/1 22:08
+ * @description:
+ */
+public class LengthFieldDecoderUsage {
+
+    public static void main(String[] args) {
+        EmbeddedChannel channel = new EmbeddedChannel(
+                /**
+                 * int maxFrameLength, int lengthFieldOffset, int lengthFieldLength, int lengthAdjustment, int initialBytesToStrip
+                 * maxFrameLength 最大帧长度
+                 * lengthFieldOffset 长度字段偏移量
+                 * lengthFieldLength 长度字段字节数
+                 * lengthAdjustment 长度字段为基准，内容数据偏移量
+                 * initialBytesToStrip 去除字节数(从头开始算)，initialBytesToStrip=lengthFieldLength+lengthAdjustment
+                 */
+                new LengthFieldBasedFrameDecoder(
+                        1024, 0, 4, 1,5),
+                new LoggingHandler(LogLevel.DEBUG)
+        );
+
+        // 4 个字节的内容长度，实际内容
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+        send(buffer, "Hello, world");
+        send(buffer, "Hi!");
+        channel.writeInbound(buffer);
+    }
+
+    private static void send(ByteBuf buffer, String content) {
+        byte[] bytes = content.getBytes(); // 实际内容
+        int length = bytes.length; // 实际内容长度
+        buffer.writeInt(length);
+        buffer.writeByte(1);
+        buffer.writeBytes(bytes);
+    }
+}
