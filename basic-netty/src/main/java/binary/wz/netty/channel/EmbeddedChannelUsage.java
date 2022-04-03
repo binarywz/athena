d5dbfaa226edb3db87851f337d98b1bcc3d92484
binarywz <binarywz@163.com>
2022-04-03 20:48:14 +0800

    netty embedded channel

---
+++ basic-netty/src/main/java/binary/wz/netty/channel/EmbeddedChannelUsage.java
@@ -0,0 +1,55 @@
+package binary.wz.netty.channel;
+
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.embedded.EmbeddedChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author binarywz
+ * @date 2022/4/3 20:41
+ * @description:
+ */
+@Slf4j
+public class EmbeddedChannelUsage {
+    public static void main(String[] args) {
+        ChannelInboundHandlerAdapter h1 = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                log.debug("1");
+                super.channelRead(ctx, msg);
+            }
+        };
+        ChannelInboundHandlerAdapter h2 = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                log.debug("2");
+                super.channelRead(ctx, msg);
+            }
+        };
+        ChannelOutboundHandlerAdapter h3 = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                log.debug("3");
+                super.write(ctx, msg, promise);
+            }
+        };
+        ChannelOutboundHandlerAdapter h4 = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                log.debug("4");
+                super.write(ctx, msg, promise);
+            }
+        };
+        EmbeddedChannel embeddedChannel = new EmbeddedChannel(h1, h2, h3, h4);
+        // 模拟入站
+        embeddedChannel.writeInbound(ByteBufAllocator.DEFAULT.buffer().writeBytes("hello netty".getBytes(StandardCharsets.UTF_8)));
+        // 模拟出站
+        embeddedChannel.writeOutbound(ByteBufAllocator.DEFAULT.buffer().writeBytes("netty hello".getBytes(StandardCharsets.UTF_8)));
+    }
+}
